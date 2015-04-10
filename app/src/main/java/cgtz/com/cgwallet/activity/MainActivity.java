f067ca1daed4c11fa1202c7846ab2bdd8d286753
{sunrui} <{sunrui@cgtz.com}>
2015-04-10 17:23:12 +0800

    添加极光推送jar包

--- app/src/main/java/cgtz/com/cgwallet/activity/MainActivity.java
+++ app/src/main/java/cgtz/com/cgwallet/activity/MainActivity.java
@@ -1,20 +1,15 @@
 package cgtz.com.cgwallet.activity;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 
 import cgtz.com.cgwallet.R;
+import cgtz.com.cgwallet.presenter.SplashPresenter;
 import cgtz.com.cgwallet.view.BidirSlidingLayout;
-import cgtz.com.cgwallet.view.CustormLayout;
 import cgtz.com.cgwallet.view.ISplashView;
 
 /**
@@ -27,6 +22,7 @@ public class MainActivity extends BaseActivity implements ISplashView{
     private ImageView showRightButton;
     private ListView contentList;
     private ArrayAdapter<String> contentListAdapter;
+    private SplashPresenter splashPresenter;
     /**
      * 用于填充contentListAdapter的数据源。
      */
@@ -39,6 +35,7 @@ public class MainActivity extends BaseActivity implements ISplashView{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        splashPresenter = new SplashPresenter(this);
         initViews();
         contentListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 contentItems);
@@ -74,6 +71,21 @@ public class MainActivity extends BaseActivity implements ISplashView{
         showLeftButton = (ImageView) findViewById(R.id.show_left_button);
         showRightButton = (ImageView) findViewById(R.id.show_right_button);
         contentList = (ListView) findViewById(R.id.contentList);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
