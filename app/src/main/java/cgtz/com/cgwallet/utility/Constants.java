954a06323d3ea4df95e04aa3ac95751b0a705c4f
{sunrui} <{sunrui@cgtz.com}>
2015-04-11 10:02:34 +0800

    添加访问服务异步线程； http方法

---
+++ app/src/main/java/cgtz/com/cgwallet/utility/Constants.java
@@ -0,0 +1,11 @@
+package cgtz.com.cgwallet.utility;
+
+/**
+ * Created by Administrator on 2015/4/11.
+ */
+public class Constants {
+    public static final boolean IS_TEST = true;//判断是否为测试环境
+    public static final String OFFLINE_HTTP = "";//线下路径
+    public static final String ONLINE_HTTP = "";//线上路径
+    public static final String VERSION = "";//版本号
+}
---
+++ app/src/main/java/cgtz/com/cgwallet/utils/CustomTask.java
@@ -0,0 +1,92 @@
+package cgtz.com.cgwallet.utils;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+
+import java.util.Map;
+
+import cgtz.com.cgwallet.utility.Constants;
+
+/**
+ * 自定义Task线程
+ * Created by Administrator on 2014/11/25.
+ */
+public class CustomTask extends AsyncTask<String,Void,String> {
+    private Handler handler;//回调Handler
+    private String url_;//访问路径
+    private boolean flag;//判断get或者post方法  get为false,post为true
+    private String content;//post传递的参数
+    private String encode;//编码
+    private int handler_what;//回调handler的 what判断值
+
+    /**
+     *
+     * @param handler
+     * @param handler_what  handler 判断值
+     * @param url_          接口
+     * @param isPost        判断是否为post访问   true为post
+     * @param maps          参数集合
+     * @param encode        文字编码
+     */
+    public CustomTask(Handler handler,int handler_what,String url_,
+                      boolean isPost,Map<String,String> maps,String encode){
+        this.handler = handler;
+        this.handler_what = handler_what;
+        this.flag = isPost;
+        this.url_ = (Constants.IS_TEST?Constants.OFFLINE_HTTP:Constants.ONLINE_HTTP) + url_;
+        this.encode = encode;
+        if(flag){
+            content = HttpUtils.getRequestData(maps,encode).toString();
+        }
+        LogUtils.e("CustomTask", "url: " + this.url_+" content: "+content);
+    }
+    /**
+     *
+     * @param handler
+     * @param handler_what  handler 判断值
+     * @param url_          接口
+     * @param isPost        判断是否为post访问   true为post
+     * @param maps          参数集合
+     * @param encode        文字编码
+     * @param if_ping       判断是否拼接路径  true为拼接
+     */
+    public CustomTask(Handler handler,int handler_what,String url_,
+                      boolean isPost,Map<String,String> maps,String encode,boolean if_ping){
+        this.handler = handler;
+        this.handler_what = handler_what;
+        this.flag = isPost;
+        if(if_ping) {
+            this.url_ = (Constants.IS_TEST?Constants.OFFLINE_HTTP:Constants.ONLINE_HTTP) + url_;
+        }else{
+            this.url_ = url_;
+        }
+        this.encode = encode;
+        if(flag){
+            content = HttpUtils.getRequestData(maps,encode).toString();
+        }
+        LogUtils.e("CustomTask", "url: " + this.url_+" content: "+content);
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        if(flag){
+            if(Constants.IS_TEST){
+                return HttpUtils.HttpPost(url_,content,encode);
+            }else{
+                return HttpUtils.HttpsPost(url_, content, encode);
+            }
+        }else{
+            if(Constants.IS_TEST){
+                return HttpUtils.HttpGet(url_, encode);
+            }else{
+                return HttpUtils.HttpsGet(url_, encode);
+            }
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        handler.sendMessage(handler.obtainMessage(handler_what,s));
+    }
+}
