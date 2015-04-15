927eb192b6ed0f8f1886fd63929c6a7d7893a8e6
{sunrui} <{sunrui@cgtz.com}>
2015-04-15 17:54:40 +0800

    实现接口

--- app/src/main/java/cgtz/com/cgwallet/activity/LoginOrRegistActivity.java
+++ app/src/main/java/cgtz/com/cgwallet/activity/LoginOrRegistActivity.java
@@ -3,11 +3,12 @@ package cgtz.com.cgwallet.activity;
 import android.os.Bundle;
 
 import cgtz.com.cgwallet.R;
+import cgtz.com.cgwallet.view.ISplashView;
 
 /**
  * 登录或注册页面
  */
-public class LoginOrRegistActivity extends BaseActivity {
+public class LoginOrRegistActivity extends BaseActivity implements ISplashView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +16,25 @@ public class LoginOrRegistActivity extends BaseActivity {
         setTitle("登录/注册");
         showBack(true);
         setContentView(R.layout.activity_login_or_regist);
+    }
+
+    @Override
+    public void startProcessBar() {
+
+    }
+
+    @Override
+    public void hideProcessBar() {
+
+    }
+
+    @Override
+    public void showNetError() {
+
+    }
+
+    @Override
+    public void startNextActivity() {
+
     }
 }
