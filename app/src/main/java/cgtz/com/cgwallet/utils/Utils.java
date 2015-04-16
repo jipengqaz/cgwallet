9d0a04f99701a2e2c4775b13486fa0b98f8eeb27
{sunrui} <{sunrui@cgtz.com}>
2015-04-16 14:50:16 +0800

    获取登录手机号方法； 增加常量

--- app/src/main/java/cgtz/com/cgwallet/utils/Utils.java
+++ app/src/main/java/cgtz/com/cgwallet/utils/Utils.java
@@ -1,7 +1,20 @@
 package cgtz.com.cgwallet.utils;
 
+import android.content.Context;
+
+import cgtz.com.cgwallet.utility.Constants;
+
 /**
  * Created by Administrator on 2015/4/10.
  */
 public class Utils {
+    /**
+     * 获取登录的手机号
+     * @param context
+     * @return
+     */
+    public static String getUserPhone(Context context){
+        SharedUtils sharedUtils = new SharedUtils(context, Constants.CONFIG);
+        return sharedUtils.getString(Constants.LOGIN_PHONE,"");
+    }
 }
