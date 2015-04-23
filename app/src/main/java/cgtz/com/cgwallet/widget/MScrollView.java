1f582ce36121503b30e316da104b8005ba14237d
{sunrui} <{sunrui@cgtz.com}>
2015-04-23 09:36:23 +0800

    自定义Scrollview

--- app/src/main/java/cgtz/com/cgwallet/widget/MScrollView.java
+++ app/src/main/java/cgtz/com/cgwallet/widget/MScrollView.java
@@ -12,26 +12,48 @@ import android.widget.ScrollView;
  * Created by Administrator on 2015/4/22.
  */
 public class MScrollView extends ScrollView {
+    private float xDistance;
+    private float yDistance;
+    private float xLast;
+    private float yLast;
     GestureDetector gestureDetector;
     public MScrollView(Context context) {
         super(context);
-        gestureDetector = new GestureDetector(context,new YScrollDetactor());
-        setFadingEdgeLength(0);
+//        gestureDetector = new GestureDetector(context,new YScrollDetactor());
+//        setFadingEdgeLength(0);
     }
     public MScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        gestureDetector = new GestureDetector(context,new YScrollDetactor());
-        setFadingEdgeLength(0);
+//        gestureDetector = new GestureDetector(context,new YScrollDetactor());
+//        setFadingEdgeLength(0);
     }
     public MScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        gestureDetector = new GestureDetector(context,new YScrollDetactor());
-        setFadingEdgeLength(0);
+//        gestureDetector = new GestureDetector(context,new YScrollDetactor());
+//        setFadingEdgeLength(0);
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return super.onInterceptTouchEvent(ev) && gestureDetector.onTouchEvent(ev);
+        int action = ev.getAction();
+        switch (action){
+            case MotionEvent.ACTION_DOWN:
+                xDistance = yDistance = 0f;
+                xLast = ev.getX();
+                yLast = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float curX = ev.getX();
+                final float cury = ev.getY();
+                xDistance += Math.abs(curX - xLast);
+                yDistance += Math.abs(cury - yLast);
+                xLast = curX;
+                yLast = cury;
+                if(xDistance > yDistance){
+                    return false;
+                }
+        }
+        return super.onInterceptTouchEvent(ev);
     }
 
     class YScrollDetactor extends GestureDetector.SimpleOnGestureListener{
