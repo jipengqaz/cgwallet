dd88e9163038edd3de57ffadcd726c058e4a9ef0
{sunrui} <{sunrui@cgtz.com}>
2015-04-25 16:43:35 +0800

    添加存钱页面

--- app/src/main/java/cgtz/com/cgwallet/activity/SaveMoneyActivity.java
+++ app/src/main/java/cgtz/com/cgwallet/activity/SaveMoneyActivity.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 
 import cgtz.com.cgwallet.R;
 
+/**
+ * 存钱页面
+ */
 public class SaveMoneyActivity extends BaseActivity {
-
+    private static final String TAG = "SaveMoneyActivity";
+    private String assets;//账户余额
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_money);
         setTitle("存钱");
         showBack(true);
+
     }
 
 }
