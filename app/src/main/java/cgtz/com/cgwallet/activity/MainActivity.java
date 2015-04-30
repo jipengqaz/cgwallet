3dfd5aa693fb365a33b3c5d5ca0e7b34c3ce78d7
zhouguotao <guotao.zhou@cgtz.com>
2015-04-30 18:04:34 +0800

    隐藏新浪分享

--- app/src/main/java/cgtz/com/cgwallet/activity/MainActivity.java
+++ app/src/main/java/cgtz/com/cgwallet/activity/MainActivity.java
@@ -10,14 +10,10 @@ import android.os.Message;
 import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,7 +44,6 @@ import java.util.ArrayList;
 import cgtz.com.cgwallet.MApplication;
 import cgtz.com.cgwallet.R;
 import cgtz.com.cgwallet.bean.JsonBean;
-import cgtz.com.cgwallet.client.Get_share_content;
 import cgtz.com.cgwallet.fragment.CgWalletFragment;
 import cgtz.com.cgwallet.fragment.MyWalletFragment;
 import cgtz.com.cgwallet.presenter.SplashPresenter;
@@ -56,7 +51,6 @@ import cgtz.com.cgwallet.utility.Constants;
 import cgtz.com.cgwallet.utils.HttpUtils;
 import cgtz.com.cgwallet.utils.LogUtils;
 import cgtz.com.cgwallet.utils.Utils;
-import cgtz.com.cgwallet.view.BidirSlidingLayout;
 import cgtz.com.cgwallet.view.ISplashView;
 import cgtz.com.cgwallet.widget.ProgressDialog;
 import cgtz.com.cgwallet.widget.SlidingMenu;
@@ -241,6 +235,7 @@ public class MainActivity extends FragmentActivity implements ISplashView,View.O
                             // 参数1为Context类型对象， 参数2为要分享到的目标平台， 参数3为分享操作的回调接口
                             mController.postShare(MainActivity.this, SHARE_MEDIA.QQ, mShareListener);
                         }else{
+                            Utils.makeToast(MainActivity.this,"您为安装QQ,请安装后分享！");
                         }
                         break;
                     case R.id.qzone:
