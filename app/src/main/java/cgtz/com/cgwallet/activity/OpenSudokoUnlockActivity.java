155477caef492c4d5547e7a9ea58ade12b348b5f
zhouguotao <guotao.zhou@cgtz.com>
2015-05-04 16:23:47 +0800

    增加取消手势密码   需要验证登录密码

--- app/src/main/java/cgtz/com/cgwallet/activity/OpenSudokoUnlockActivity.java
+++ app/src/main/java/cgtz/com/cgwallet/activity/OpenSudokoUnlockActivity.java
@@ -4,14 +4,23 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import cgtz.com.cgwallet.MApplication;
 import cgtz.com.cgwallet.R;
+import cgtz.com.cgwallet.bean.JsonBean;
+import cgtz.com.cgwallet.client.Is_passwrod;
 import cgtz.com.cgwallet.utils.AppUtil;
 import cgtz.com.cgwallet.utils.Utils;
 
@@ -25,9 +34,9 @@ public class OpenSudokoUnlockActivity extends BaseActivity {
     private LinearLayout tv_againSet;
     private boolean flag;
     private boolean mIsDestroy = false;//判断Activity是否被关闭，防止系统销毁了Activity，还去加载Dialog的错误
-    private Dialog mDialog;
     private LayoutInflater mInflater;
     private TextView errorHint;//输入登录密码错误提示
+    private static Dialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +58,7 @@ public class OpenSudokoUnlockActivity extends BaseActivity {
         tv_againSet = (LinearLayout) findViewById(R.id.tv_open_sudoko_unlock_again);
         flag = Utils.getLockPassword(this,Utils.getUserPhone(this))!="";
 
-        if(flag){
+        if(!flag){
             tv_openLock.setImageResource(R.mipmap.lock_open);
             tv_againSet.setVisibility(View.VISIBLE);
         }else{
@@ -67,11 +76,31 @@ public class OpenSudokoUnlockActivity extends BaseActivity {
         tv_openLock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(flag){
-                Intent intent = new Intent(OpenSudokoUnlockActivity.this,GestureEditActivity.class);
-                startActivity(intent);
-                }else{
-                    Utils.removePassWord(OpenSudokoUnlockActivity.this,Utils.getUserPhone(OpenSudokoUnlockActivity.this));
+                flag = Utils.getLockPassword(OpenSudokoUnlockActivity.this, Utils.getUserPhone(OpenSudokoUnlockActivity.this)) != "";
+                Log.e("aaaa", flag + "");
+                if (!flag) {
+                    Intent intent = new Intent(OpenSudokoUnlockActivity.this, GestureEditActivity.class);
+                    startActivity(intent);
+                } else {
+                    mDialog = new Dialog(OpenSudokoUnlockActivity.this,
+                            R.style.loading_dialog2);//填写登录密码提示框
+                    View linearLayout = mInflater.inflate(R.layout.sodoko_unlock_pwd_dialog, null);
+                    TextView login_phone = (TextView) linearLayout.findViewById(R.id.tv_sodoko_login_phone);
+                    final EditText login_pwd = (EditText) linearLayout.findViewById(R.id.et_sodoko_login_pwd);
+                    TextView confirm = (TextView) linearLayout.findViewById(R.id.tv_sodoko_confirm);
+                    errorHint = (TextView) linearLayout.findViewById(R.id.tv_sodoko_error_hint);
+                    mDialog.setContentView(linearLayout);
+//        mDialog.setCanceledOnTouchOutside(false);
+                    mDialog.show();
+                    login_phone.setText(Utils.getProtectedMobile(Utils.getUserPhone(OpenSudokoUnlockActivity.this)));
+                    confirm.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            errorHint.setVisibility(View.VISIBLE);
+                            errorHint.setText("正在判断登录密码...");
+                            Is_passwrod.isPasswrod(mHandler, login_pwd.getText().toString(), 0);
+                        }
+                    });
                 }
             }
         });
@@ -86,6 +115,29 @@ public class OpenSudokoUnlockActivity extends BaseActivity {
         });
     }
 
+    /**
+     * 用于取消手势时判断登录密码的返回参数
+     */
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            JsonBean jsonBean = (JsonBean) msg.obj;
+            try {
+                JSONObject json = new JSONObject(jsonBean.getJsonString());
+                String status = json.getString("success");
+                if ("0".equals(status)) {
+                    errorHint.setText(json.optString("msg"));
+                }else{
+                    Utils.removePassWord(OpenSudokoUnlockActivity.this,Utils.getUserPhone(OpenSudokoUnlockActivity.this));
+                    tv_openLock.setImageResource(R.mipmap.lock_close);
+                    tv_againSet.setVisibility(View.GONE);
+                    mDialog.dismiss();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    };
     @Override
     protected void onResume() {
         super.onResume();
@@ -117,7 +169,6 @@ public class OpenSudokoUnlockActivity extends BaseActivity {
         if(mIsDestroy){
             return;
         }
-        AppUtil.isPasswroid(this,mInflater,false);
+        AppUtil.isPasswroid(this, mInflater, false);
     }
-
 }
